f9003ea93aa3f97019927b1909840b4402e49048
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-05 14:51:13 +0100

    Fix router config

--- nixos-configs/router-network.nix
+++ nixos-configs/router-network.nix
@@ -3,27 +3,27 @@
   networking = {
     interfaces.enp0s8 = {
       ipv4.addresses = [
-        { address = "192.168.1.3"; prefixLength = 24; }
-        { address = "192.168.2.3"; prefixLength = 24; }
         { address = "192.168.3.3"; prefixLength = 24; }
-        { address = "192.168.4.3"; prefixLength = 24; }
-        { address = "192.168.5.3"; prefixLength = 24; }
-        { address = "192.168.6.3"; prefixLength = 24; }
-        { address = "192.168.7.3"; prefixLength = 24; }
-        { address = "192.168.8.3"; prefixLength = 24; }
-        { address = "192.168.9.3"; prefixLength = 24; }
-        { address = "192.168.10.3"; prefixLength = 24; }
+        { address = "192.168.31.3"; prefixLength = 24; }
+        { address = "192.168.32.3"; prefixLength = 24; }
+        { address = "192.168.43.3"; prefixLength = 24; }
+        { address = "192.168.53.3"; prefixLength = 24; }
+        { address = "192.168.63.3"; prefixLength = 24; }
+        { address = "192.168.73.3"; prefixLength = 24; }
+        { address = "192.168.83.3"; prefixLength = 24; }
+        { address = "192.168.93.3"; prefixLength = 24; }
+        { address = "192.168.103.3"; prefixLength = 24; }
       ];
       ipv4.routes = [
-        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.3.1"; } # ids der anderen router vms
-        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.3.2"; }
-        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.4.4"; }
-        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.5.5"; }
-        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.6.6"; }
-        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.7.7"; }
-        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.8.8"; }
-        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.9.8"; }
-        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.10.9"; }
+        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.31.1"; } # ids der anderen router vms
+        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.32.2"; }
+        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.41.4"; }
+        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.51.5"; }
+        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.61.6"; }
+        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.71.7"; }
+        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.81.8"; }
+        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.91.8"; }
+        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.101.9"; }
       ];
     };
 
