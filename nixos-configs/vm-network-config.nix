195824f64b668f11d33d81db957359a29c9fbf36
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-05 19:57:05 +0100

    Try vm config

--- nixos-configs/vm-network-config.nix
+++ nixos-configs/vm-network-config.nix
@@ -6,15 +6,7 @@
         { address = "192.168.3.%%vm%%"; prefixLength = 24; }
       ];
       ipv4.routes = [
-        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.31.3"; }
-        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.32.3"; }
-        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.41.3"; }
-        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.51.3"; }
-        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.61.3"; }
-        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.71.3"; }
-        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.81.3"; }
-        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.91.3"; }
-        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.101.3"; }
+        { address = "192.168.0.0"; prefixLength = 16; via = "192.168.3.3"; }
       ];
     };
 
