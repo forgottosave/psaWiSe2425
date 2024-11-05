d7e06bebc8271872c9557ecf7ad5c1dab15a9581
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-05 14:16:44 +0100

    fix router ips

--- nixos-configs/router-network.nix
+++ nixos-configs/router-network.nix
@@ -3,27 +3,27 @@
   networking = {
     interfaces.enp0s8 = {
       ipv4.addresses = [
+        { address = "192.168.1.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.2.%%vm%%"; prefixLength = 24; }
         { address = "192.168.3.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.31.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.32.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.43.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.53.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.63.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.73.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.83.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.93.%%vm%%"; prefixLength = 24; }
-        { address = "192.168.103.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.4.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.5.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.6.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.7.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.8.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.9.%%vm%%"; prefixLength = 24; }
+        { address = "192.168.10.%%vm%%"; prefixLength = 24; }
       ];
       ipv4.routes = [
-        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.31.1"; } # ids der anderen router vms
-        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.32.2"; }
-        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.41.4"; }
-        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.51.5"; }
-        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.61.6"; }
-        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.71.7"; }
-        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.81.8"; }
-        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.91.8"; }
-        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.101.9"; }
+        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.3.1"; } # ids der anderen router vms
+        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.3.2"; }
+        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.4.4"; }
+        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.5.5"; }
+        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.6.6"; }
+        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.7.7"; }
+        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.8.8"; }
+        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.9.8"; }
+        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.10.9"; }
       ];
     };
 
@@ -90,24 +90,24 @@
       iptables -A OUTPUT -d 192.168.10.0/24 -j ACCEPT
 
       # Allow: Forrwarding between the networks
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.1.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.1.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.1.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.1.0/24 -d 192.168.3.0/24 -j ACCEPT
       iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.4.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.4.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.5.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.5.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.6.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.6.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.7.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.7.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.8.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.8.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.9.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.9.0/24 -d 192.168.2.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.10.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.10.0/24 -d 192.168.2.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.2.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.4.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.4.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.5.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.5.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.6.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.6.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.7.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.7.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.8.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.8.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.9.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.9.0/24 -d 192.168.3.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.10.0/24 -j ACCEPT
+      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.10.0/24 -d 192.168.3.0/24 -j ACCEPT
 
       # Allow: ICMP 
       iptables -A INPUT -p icmp -j ACCEPT
