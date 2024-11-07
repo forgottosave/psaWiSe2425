5fd39e0e058d0acef7528156c07873c331e0c330
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-07 13:26:48 +0100

    fix vm-network-config.nix

--- nixos-configs/vm-network-config.nix
+++ nixos-configs/vm-network-config.nix
@@ -2,7 +2,7 @@
 {
   networking = {
     firewall.enable = false;
-    
+
     interfaces.enp0s8 = {
       ipv4.addresses = [
         { address = "192.168.3.%%vm%%"; prefixLength = 24; }
@@ -11,5 +11,5 @@
         { address = "192.168.0.0"; prefixLength = 16; via = "192.168.3.3"; }
       ];
     };
-  }
+  };
 }
