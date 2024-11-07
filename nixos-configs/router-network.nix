e28095740267e96144d9af894d94ab855a781a24
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-07 11:19:40 +0100

    TEST change router config: networking -> networks

--- nixos-configs/router-network.nix
+++ nixos-configs/router-network.nix
@@ -1,124 +1,184 @@
 {config, pkgs, ... }: 
 {
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.forwarding" = true;
-  };
+  # Legacy DHCP deaktivieren
+  networking.useDHCP = false;
 
-  networking = {
-    useDHCP = false;
-    nat.enable = false;
+  # Wir nutzen stattdessen systemd-networkd
+  systemd.network = {
+    enable = true;
 
-    interfaces.enp0s8 = {
-      ipv4.addresses = [
-        { address = "192.168.3.3"; prefixLength = 24; }
-        { address = "192.168.31.3"; prefixLength = 24; }
-        { address = "192.168.32.3"; prefixLength = 24; }
-        { address = "192.168.43.3"; prefixLength = 24; }
-        { address = "192.168.53.3"; prefixLength = 24; }
-        { address = "192.168.63.3"; prefixLength = 24; }
-        { address = "192.168.73.3"; prefixLength = 24; }
-        { address = "192.168.83.3"; prefixLength = 24; }
-        { address = "192.168.93.3"; prefixLength = 24; }
-        { address = "192.168.103.3"; prefixLength = 24; }
-      ];
-      ipv4.routes = [
-        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.31.1"; } # ids der anderen router vms
-        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.32.2"; }
-        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.43.4"; }
-        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.53.5"; }
-        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.63.6"; }
-        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.73.7"; }
-        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.83.8"; }
-        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.93.9"; }
-        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.103.10"; }
-      ];
+    # Interface für Internet Zugang
+    networks."3-internet" = {
+      name = "enp0s3";
+      DHCP = "yes";
     };
 
-    proxy.httpsProxy = "http://proxy.cit.tum.de:8080/"; 
-    proxy.httpProxy = "http://proxy.cit.tum.de:8080/";
+    # Interface für internen PSA Zugang
+    networks."3-psa" = {
+      name = "enp0s8";
+      address = [
+        "192.168.3.3" # Team Subnet
+        "192.168.31.3"
+        "192.168.32.3"
+        "192.168.43.3"
+        "192.168.53.3"
+        "192.168.63.3"
+        "192.168.73.3"
+        "192.168.83.3"
+        "192.168.93.3"
+        "192.168.103.3"
+      ];
+      routes = [
+        { routeConfig = { Destination = "192.168.1.0/24"; Gateway = "192.168.31.1"; }; }
+        { routeConfig = { Destination = "192.168.2.0/24"; Gateway = "192.168.32.2"; }; }
+        { routeConfig = { Destination = "192.168.4.0/24"; Gateway = "192.168.43.4"; }; }
+        { routeConfig = { Destination = "192.168.5.0/24"; Gateway = "192.168.53.5"; }; }
+        { routeConfig = { Destination = "192.168.6.0/24"; Gateway = "192.168.63.6"; }; }
+        { routeConfig = { Destination = "192.168.7.0/24"; Gateway = "192.168.73.7"; }; }
+        { routeConfig = { Destination = "192.168.8.0/24"; Gateway = "192.168.83.8"; }; }
+        { routeConfig = { Destination = "192.168.9.0/24"; Gateway = "192.168.93.9"; }; }
+        { routeConfig = { Destination = "192.168.10.0/24"; Gateway = "192.168.103.10"; }; }
+      ];
+      # Enable IPv4 (&IPv6) Forwarding
+      networkConfig.IPForward = "yes";
+    };
+  };
 
-    firewall.extraCommands = '' 
-      # by default: Disable connection tracking
-      iptables -t raw -A PREROUTING -p tcp --dport 80 -j NOTRACK  
-      iptables -t raw -A OUTPUT -p tcp --sport 80 -j NOTRACK
-      iptables -t raw -A PREROUTING -p tcp --dport 443 -j NOTRACK 
-      iptables -t raw -A OUTPUT -p tcp --sport 443 -j NOTRACK
-
-      # by default: Drop all packages (in and outgoing) 
-      iptables -P INPUT DROP 
-      iptables -P FORWARD DROP 
-      iptables -P OUTPUT DROP
-
-      # Allow: loopback
-      iptables -A INPUT -i lo -j ACCEPT
-      iptables -A OUTPUT -o lo -j ACCEPT
-
-      # Allow: established connections
-      iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT    
-      iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-      # Allow: SSH
-      iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-      iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-      # Allow: DNS
-      iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-      iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-      iptables -A INPUT -p udp --dport 53 -j ACCEPT 
-      iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-
-      # Allow: git (https://serverfault.com/questions/682373/setting-up-iptables-filter-to-allow-git)
-      iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-      iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-      iptables -A INPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-      iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-
-      # Allow incoming HTTP, HTTPS, and responses to the requests
-      iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-      iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
-      iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-      iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
-
-      # Outgoing only to specific IPs
-      iptables -A OUTPUT -d 151.101.2.217 -j ACCEPT
-      iptables -A OUTPUT -d 151.101.130.217 -j ACCEPT
-      iptables -A OUTPUT -d 151.101.66.217 -j ACCEPT
-      iptables -A OUTPUT -d 151.101.194.217 -j ACCEPT
-      iptables -A OUTPUT -d 131.159.0.0/16 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.2.0/24 -j ACCEPT 
-      iptables -A OUTPUT -d 192.168.3.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.4.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.5.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.6.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.7.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.8.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.9.0/24 -j ACCEPT
-      iptables -A OUTPUT -d 192.168.10.0/24 -j ACCEPT
-
-      # Allow: Forrwarding between the networks
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.31.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.31.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.32.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.32.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.43.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.43.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.53.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.53.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.63.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.63.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.73.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.73.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.83.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.83.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.93.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.93.0/24 -d 192.168.3.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.103.0/24 -j ACCEPT
-      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.103.0/24 -d 192.168.3.0/24 -j ACCEPT
-
-      # Allow: ICMP 
-      iptables -A INPUT -p icmp -j ACCEPT
-      iptables -A OUTPUT -p icmp -j ACCEPT
-    '';
+  # ICMP Redirects deaktivieren
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.send_redirects" = false;
+    "net.ipv4.conf.default.send_redirects" = false;
+    "net.ipv6.conf.all.send_redirects" = false;
+    "net.ipv6.conf.default.send_redirects" = false;
+    "net.ipv4.conf.all.accept_redirects" = false;
+    "net.ipv4.conf.default.accept_redirects" = false;
+    "net.ipv6.conf.all.accept_redirects" = false;
+    "net.ipv6.conf.default.accept_redirects" = false;
   };
 }
+
+
+
+
+#  boot.kernel.sysctl = {
+#    "net.ipv4.conf.all.forwarding" = true;
+#  };
+#
+#  networking = {
+#    useDHCP = false;
+#    nat.enable = false;
+#
+#    interfaces.enp0s8 = {
+#      ipv4.addresses = [
+#        { address = "192.168.3.3"; prefixLength = 24; }
+#        { address = "192.168.31.3"; prefixLength = 24; }
+#        { address = "192.168.32.3"; prefixLength = 24; }
+#        { address = "192.168.43.3"; prefixLength = 24; }
+#        { address = "192.168.53.3"; prefixLength = 24; }
+#        { address = "192.168.63.3"; prefixLength = 24; }
+#        { address = "192.168.73.3"; prefixLength = 24; }
+#        { address = "192.168.83.3"; prefixLength = 24; }
+#        { address = "192.168.93.3"; prefixLength = 24; }
+#        { address = "192.168.103.3"; prefixLength = 24; }
+#      ];
+#      ipv4.routes = [
+#        { address = "192.168.1.0"; prefixLength = 24; via = "192.168.31.1"; } # ids der anderen router vms
+#        { address = "192.168.2.0"; prefixLength = 24; via = "192.168.32.2"; }
+#        { address = "192.168.4.0"; prefixLength = 24; via = "192.168.43.4"; }
+#        { address = "192.168.5.0"; prefixLength = 24; via = "192.168.53.5"; }
+#        { address = "192.168.6.0"; prefixLength = 24; via = "192.168.63.6"; }
+#        { address = "192.168.7.0"; prefixLength = 24; via = "192.168.73.7"; }
+#        { address = "192.168.8.0"; prefixLength = 24; via = "192.168.83.8"; }
+#        { address = "192.168.9.0"; prefixLength = 24; via = "192.168.93.9"; }
+#        { address = "192.168.10.0"; prefixLength = 24; via = "192.168.103.10"; }
+#      ];
+#    };
+#
+#    proxy.httpsProxy = "http://proxy.cit.tum.de:8080/"; 
+#    proxy.httpProxy = "http://proxy.cit.tum.de:8080/";
+#
+#    firewall.extraCommands = '' 
+#      # by default: Disable connection tracking
+#      iptables -t raw -A PREROUTING -p tcp --dport 80 -j NOTRACK  
+#      iptables -t raw -A OUTPUT -p tcp --sport 80 -j NOTRACK
+#      iptables -t raw -A PREROUTING -p tcp --dport 443 -j NOTRACK 
+#      iptables -t raw -A OUTPUT -p tcp --sport 443 -j NOTRACK
+#
+#      # by default: Drop all packages (in and outgoing) 
+#      iptables -P INPUT DROP 
+#      iptables -P FORWARD DROP 
+#      iptables -P OUTPUT DROP
+#
+#      # Allow: loopback
+#      iptables -A INPUT -i lo -j ACCEPT
+#      iptables -A OUTPUT -o lo -j ACCEPT
+#
+#      # Allow: established connections
+#      iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT    
+#      iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+#
+#      # Allow: SSH
+#      iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+#      iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+#
+#      # Allow: DNS
+#      iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+#      iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+#      iptables -A INPUT -p udp --dport 53 -j ACCEPT 
+#      iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+#
+#      # Allow: git (https://serverfault.com/questions/682373/setting-up-iptables-filter-to-allow-git)
+#      iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+#      iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+#      iptables -A INPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+#      iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+#
+#      # Allow incoming HTTP, HTTPS, and responses to the requests
+#      iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+#      iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
+#      iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+#      iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
+#
+#      # Outgoing only to specific IPs
+#      iptables -A OUTPUT -d 151.101.2.217 -j ACCEPT
+#      iptables -A OUTPUT -d 151.101.130.217 -j ACCEPT
+#      iptables -A OUTPUT -d 151.101.66.217 -j ACCEPT
+#      iptables -A OUTPUT -d 151.101.194.217 -j ACCEPT
+#      iptables -A OUTPUT -d 131.159.0.0/16 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.2.0/24 -j ACCEPT 
+#      iptables -A OUTPUT -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.4.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.5.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.6.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.7.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.8.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.9.0/24 -j ACCEPT
+#      iptables -A OUTPUT -d 192.168.10.0/24 -j ACCEPT
+#
+#      # Allow: Forrwarding between the networks
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.31.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.31.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.32.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.32.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.43.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.43.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.53.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.53.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.63.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.63.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.73.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.73.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.83.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.83.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.93.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.93.0/24 -d 192.168.3.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.3.0/24 -d 192.168.103.0/24 -j ACCEPT
+#      iptables -A FORWARD -i enp0s8 -o enp0s8 -s 192.168.103.0/24 -d 192.168.3.0/24 -j ACCEPT
+#
+#      # Allow: ICMP 
+#      iptables -A INPUT -p icmp -j ACCEPT
+#      iptables -A OUTPUT -p icmp -j ACCEPT
+#    '';
+#  };
+#}
