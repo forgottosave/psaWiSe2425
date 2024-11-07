16ba4d25fb75175ff61980608dc7fbb0d97c2ae5
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-07 15:56:14 +0100

    enable firewall (not configured yet) and proxy

--- nixos-configs/router-network.nix
+++ nixos-configs/router-network.nix
@@ -3,7 +3,7 @@
   networking = {
     useDHCP = false;
     nat.enable = false;
-    firewall.enable = false;
+    firewall.enable = true;
 
     interfaces.enp0s8 = {
       ipv4.addresses = [
@@ -30,6 +30,10 @@
         { address = "192.168.10.0"; prefixLength = 24; via = "192.168.103.10"; }
       ];
     };
+
+    proxy.httpsProxy = "http://proxy.cit.tum.de:8080/"; 
+    proxy.httpProxy = "http://proxy.cit.tum.de:8080/";
+
   };
 
   # Forwarding aktivieren, ICMP Redirects deaktivieren
@@ -48,52 +52,3 @@
     "net.ipv6.conf.default.accept_redirects" = false;
   };
 }
-
-
-#  # Legacy DHCP deaktivieren
-#  networking.nat.enable = false;
-#  networking.useDHCP = false;
-#
-#  # Wir nutzen stattdessen systemd-networkd
-#  networking.useNetworkd = true;
-#  systemd.network = {
-#    enable = true;
-#
-#    # Interface für Internet Zugang
-#    networks."10-internet" = {
-#      name = "enp0s3";
-#      DHCP = "yes";
-#    };
-#
-#    # Interface für internen PSA Zugang
-#    networks."10-psa" = {
-#      name = "enp0s8";
-#      address = [
-#        "192.168.3.3" # Team Subnet
-#        "192.168.31.3"
-#        "192.168.32.3"
-#        "192.168.43.3"
-#        "192.168.53.3"
-#        "192.168.63.3"
-#        "192.168.73.3"
-#        "192.168.83.3"
-#        "192.168.93.3"
-#        "192.168.103.3"
-#      ];
-#      routes = [
-#        { routeConfig = { Destination = "192.168.1.0/24"; Gateway = "192.168.31.1"; }; }
-#        { routeConfig = { Destination = "192.168.2.0/24"; Gateway = "192.168.32.2"; }; }
-#        { routeConfig = { Destination = "192.168.4.0/24"; Gateway = "192.168.43.4"; }; }
-#        { routeConfig = { Destination = "192.168.5.0/24"; Gateway = "192.168.53.5"; }; }
-#        { routeConfig = { Destination = "192.168.6.0/24"; Gateway = "192.168.63.6"; }; }
-#        { routeConfig = { Destination = "192.168.7.0/24"; Gateway = "192.168.73.7"; }; }
-#        { routeConfig = { Destination = "192.168.8.0/24"; Gateway = "192.168.83.8"; }; }
-#        { routeConfig = { Destination = "192.168.9.0/24"; Gateway = "192.168.93.9"; }; }
-#        { routeConfig = { Destination = "192.168.10.0/24"; Gateway = "192.168.103.10"; }; }
-#      ];
-#      # Enable IPv4 (&IPv6) Forwarding
-#      networkConfig.IPForward = "yes";
-#    };
-#  };
-#
-#}
