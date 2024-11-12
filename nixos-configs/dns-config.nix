1083d2bf58fd6c09f03326ac50b06b537d169d2a
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-12 14:56:34 +0100

    #comment reverse lookup

--- nixos-configs/dns-config.nix
+++ nixos-configs/dns-config.nix
@@ -28,28 +28,28 @@
         '';
       };
       # OUR TEAM - reverse
-      "3.168.192.in-addr.arpa" = {
-        master = true;
-        file = pkgs.writeText "zone-3.168.192.in-addr.arpa" ''
-          $TTL    1h
-          @            IN      SOA     router hostmaster (
-                                           1    ; Serial
-                                           3h   ; Refresh
-                                           1h   ; Retry
-                                           1w   ; Expire
-                                           1h)  ; Negative Cache TTL
-                       IN      NS      vm1
-                       IN      NS      vm2
-
-          @            IN      A       router.psa-team03.cit.tum.de
-
-          3          IN      A       router.psa-team03.cit.tum.de
-
-          1          IN      A       vm1.psa-team03.cit.tum.de
-
-          2          IN      A       vm2.psa-team03.cit.tum.de
-        '';
-      };
+      #"3.168.192.in-addr.arpa" = {
+      #  master = true;
+      #  file = pkgs.writeText "zone-3.168.192.in-addr.arpa" ''
+      #    $TTL    1h
+      #    @            IN      SOA     router hostmaster (
+      #                                     1    ; Serial
+      #                                     3h   ; Refresh
+      #                                     1h   ; Retry
+      #                                     1w   ; Expire
+      #                                     1h)  ; Negative Cache TTL
+      #                 IN      NS      vm1
+      #                 IN      NS      vm2
+      #
+      #    @            IN      A       router.psa-team03.cit.tum.de
+      #
+      #    3          IN      A       router.psa-team03.cit.tum.de
+      #
+      #    1          IN      A       vm1.psa-team03.cit.tum.de
+      #
+      #    2          IN      A       vm2.psa-team03.cit.tum.de
+      #  '';
+      #};
     };
   };
 }
