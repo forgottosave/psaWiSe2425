4293b22c81da915820b4c934e6c35ae5e4fdb5c9
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-12 15:17:36 +0100

    re-add reverse DNS

--- nixos-configs/dns-config.nix
+++ nixos-configs/dns-config.nix
@@ -27,29 +27,29 @@
           vm2          IN      A       192.168.3.2
         '';
       };
-      # OUR TEAM - reverse
-      #"3.168.192.in-addr.arpa" = {
-      #  master = true;
-      #  file = pkgs.writeText "zone-3.168.192.in-addr.arpa" ''
-      #    $TTL    1h
-      #    @            IN      SOA     router hostmaster (
-      #                                     1    ; Serial
-      #                                     3h   ; Refresh
-      #                                     1h   ; Retry
-      #                                     1w   ; Expire
-      #                                     1h)  ; Negative Cache TTL
-      #                 IN      NS      vm1
-      #                 IN      NS      vm2
-      #
-      #    @            IN      A       router.psa-team03.cit.tum.de
-      #
-      #    3          IN      A       router.psa-team03.cit.tum.de
-      #
-      #    1          IN      A       vm1.psa-team03.cit.tum.de
-      #
-      #    2          IN      A       vm2.psa-team03.cit.tum.de
-      #  '';
-      #};
+       OUR TEAM - reverse
+      "3.168.192.in-addr.arpa" = {
+        master = true;
+        file = pkgs.writeText "zone-3.168.192.in-addr.arpa" ''
+          $TTL    1h
+          @            IN      SOA     router hostmaster (
+                                           1    ; Serial
+                                           3h   ; Refresh
+                                           1h   ; Retry
+                                           1w   ; Expire
+                                           1h)  ; Negative Cache TTL
+                       IN      NS      vm1
+                       IN      NS      vm2
+      
+          @            IN      A       router.psa-team03.cit.tum.de
+      
+          3          IN      A       router.psa-team03.cit.tum.de
+      
+          1          IN      A       vm1.psa-team03.cit.tum.de
+      
+          2          IN      A       vm2.psa-team03.cit.tum.de
+        '';
+      };
     };
   };
 }
