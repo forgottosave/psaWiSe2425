23b537a357e9f903aad8301a0cfc4ec2f3742bef
Benjamin Liertz <benjamin.liertz@gmx.net>
2024-11-14 11:50:16 +0100

    fix structure

--- nixos-configs/dhcp-config.nix
+++ nixos-configs/dhcp-config.nix
@@ -3,11 +3,13 @@
   services.kea.dhcp4.enable = true;
   services.kea.dhcp4.settings = {
 
+    # Host identifizierung über mac addresse
     "host-reservation-identifiers" =  [ "hw-address" ];
 
     # DHCP soll nicht antworten, falls angefragte IP nicht verfügbar
     "authoritative" = false;
 
+    # Interfaces, auf denen der DHCP-Server arbeitet
     "interfaces-config" = {
       "interfaces" = [ "enp0s8" ];
       # "dhcp-socket-type" = "raw";
@@ -25,34 +27,6 @@
         ];
         # "client-class" = "vm1"; # TODO vm2
 
-
-        "option-def" = [
-            {
-                "code" = 252;
-                "name" = "wpad-proxy-url";
-                "type" = "string";
-            }
-        ];
-
-
-        # Domain-Informationen und Routen
-        "option-data" = [
-          { "name" = "domain-name"; "data" = "psa-team03.cit.tum.de"; "always-send" = true; }
-          { "name" = "domain-name-servers"; "data" = "192.168.3.3"; "always-send" = true; }
-          { "name" = "routers"; "data" = "192.168.3.3"; "always-send" = true; }
-          # { "name" = "netmask"; "data" = "255.255.255.0"; } # auto calculated
-           { "name" = "wpad-proxy-url"; "data" = "http://pac.lrz.de"; "always-send" = true; }
-          # https://gitlab.isc.org/isc-projects/kea/-/merge_requests/2135
-          #{
-          #  "code" = 121;
-          #  "name" = "classless-static-route";
-          #  "data" = "192.168.1.0/24 - 192.168.31.1, 192.168.2.0/24 - 192.168.32.1, 192.168.4.0/24 - 192.168.43.1, 192.168.5.0/24 - 192.168.53.1, 192.168.6.0/24 - 192.168.63.1, 192.168.7.0/24 - 192.168.73.1, 192.168.8.0/24 - 192.168.83.1, 192.168.9.0/24 - 192.168.93.1, 192.168.10.0/24 - 192.168.103.1";
-          #  "always-send" = true;
-          #}
-        ];
-
-        # static IP-Adressen für vm1 & vm2
-        #https://kb.isc.org/docs/what-are-host-reservations-how-to-use-them
         "reservations" = [
           {
             "hw-address" = "08:00:27:7a:a5:78"; 
@@ -67,6 +41,34 @@
         ];
       }
     ];
+
+
+    # definition einer option für wpad-proxy-url
+    "option-def" = [
+        {
+            "code" = 252;
+            "name" = "wpad-proxy-url";
+            "type" = "string";
+        }
+    ];
+
+
+    # Domain-Informationen und Routen
+    "option-data" = [
+      { "name" = "domain-name"; "data" = "psa-team03.cit.tum.de"; "always-send" = true; }
+      { "name" = "domain-name-servers"; "data" = "192.168.3.3"; "always-send" = true; }
+      { "name" = "routers"; "data" = "192.168.3.3"; "always-send" = true; }
+      # { "name" = "netmask"; "data" = "255.255.255.0"; } # auto calculated
+        { "name" = "wpad-proxy-url"; "data" = "http://pac.lrz.de"; "always-send" = true; }
+      # https://gitlab.isc.org/isc-projects/kea/-/merge_requests/2135
+      #{
+      #  "code" = 121;
+      #  "name" = "classless-static-route";
+      #  "data" = "192.168.1.0/24 - 192.168.31.1, 192.168.2.0/24 - 192.168.32.1, 192.168.4.0/24 - 192.168.43.1, 192.168.5.0/24 - 192.168.53.1, 192.168.6.0/24 - 192.168.63.1, 192.168.7.0/24 - 192.168.73.1, 192.168.8.0/24 - 192.168.83.1, 192.168.9.0/24 - 192.168.93.1, 192.168.10.0/24 - 192.168.103.1";
+      #  "always-send" = true;
+      #}
+    ];
+
 
     # Steuerung von Anfragen, um nur auf bestimmte Clients zu antworten
     # https://kb.isc.org/docs/understanding-client-classification
