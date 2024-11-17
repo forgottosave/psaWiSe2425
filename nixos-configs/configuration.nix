23f57d074917c3ff1ca3755f06cc6be4ba42c695
Benjamin Liertz <benjamin.liertz@gmx.net>
2024-11-17 16:32:32 +0100

    enable flakes

--- nixos-configs/configuration.nix
+++ nixos-configs/configuration.nix
@@ -47,6 +47,8 @@
   ];
   programs.mtr.enable = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
