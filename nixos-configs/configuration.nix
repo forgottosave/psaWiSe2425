44fe0b518ce5d7f008fcb37cb1ac0e79178241e7
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-19 12:22:28 +0100

    fix ssh config

--- nixos-configs/configuration.nix
+++ nixos-configs/configuration.nix
@@ -18,8 +18,7 @@
   services.sshd.enable = true;
   services.openssh = {
       enable = true;                                  # Enable the OpenSSH daemon
-      PermitRootLogin = "prohibit-password";          # Disable root passwd login
-      PasswordAuthentication = false;                 # Disable password authentication
+      settings.PasswordAuthentication = false;                 # Disable password authentication
       settings.KbdInteractiveAuthentication = false;  # Disable keyboard-interactive authentication
       settings.PermitRootLogin = "yes";               # Enable root login
   };
