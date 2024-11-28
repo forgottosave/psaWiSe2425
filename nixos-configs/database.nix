31dd5abfa6cf93a6bf6dc40ffdba00a28d93c021
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-28 12:05:28 +0100

    change service subnet

--- nixos-configs/database.nix
+++ nixos-configs/database.nix
@@ -43,7 +43,7 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method                  optional_ident_map
       local all       all     peer                         map=superuser_map
-      host  team02    team02  127.0.0.1/32 scram-sha-256
+      host  team02    team02  192.168.0.0/16 scram-sha-256
     '';
     # Users & Databases
     initialScript = pkgs.writeText "backend-initScript" ''
