41e84f1c1d385935845591f6aac56a54b1eeab13
Timon Ensel <forgottosave.ensel@tum.de>
2024-12-01 20:12:39 +0100

    change backup time to 20:15 for testing

--- nixos-configs/database.nix
+++ nixos-configs/database.nix
@@ -59,7 +59,7 @@
   ## BACKUP SETUP
   services.postgresqlBackup = {
     enable = true;
-    startAt = "*-*-* 01:15:00";
+    startAt = "*-*-* 20:15:00";
     location = "/root/database_backups/";
     backupAll = true;
     compression = "gzip";
