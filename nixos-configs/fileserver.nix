fd19153068c97089b4a01a6c14886d85f20a1b9d
Timon Ensel <forgottosave.ensel@tum.de>
2025-01-04 17:31:33 +0100

    add users and nfs config

--- nixos-configs/fileserver.nix
+++ nixos-configs/fileserver.nix
@@ -1,14 +1,35 @@
 { config, lib, pkgs, ... }:
 {
-  boot.swraid = {
-    enable = true;
-    mdadmConf = ''
-      ARRAY /dev/md127 metadata=1.2 UUID=d6e68de0:d5c8423f:715c48b0:f61316f8
-    '';
+  # TODO Mount RAID at /export
+  #boot.swraid = {
+  #  enable = true;
+  #  mdadmConf = ''
+  #    ARRAY /dev/md127 metadata=1.2 UUID=d6e68de0:d5c8423f:715c48b0:f61316f8
+  #  '';
+  #};
+  #
+  #fileSystems."/export" = {
+  #  device = "/dev/md127";
+  #  fsType = "ext4";
+  #};
+
+  # add postgres user manually
+  users.groups.postgres.gid = 71;
+  users.users.postgres = {
+    isSystemUser = true;
+    home = "/home/postgres";
+    uid = 71; # according to VM 4 postgres user id
+    group = "postgres"; 
   };
 
-  fileSystems."/export" = {
-    device = "/dev/md127";
-    fsType = "ext4";
+  # enable nfs server
+  services.nfs.server = {
+    enable = true;
+    createMountPoints = true;
+    exports = ''
+      /export                 192.168.0.0/16(rw,fsid=0,no_subtree_check)
+      /export/home            192.168.0.0/16(rw,sync)
+      /export/postgresql      192.168.3.4(rw,sync)
+    '';
   };
 }
