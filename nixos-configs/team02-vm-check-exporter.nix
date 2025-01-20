c5ddcbdfe587d956c8e1855e00b4eb848669acbe
Timon Ensel <forgottosave.ensel@tum.de>
2025-01-20 18:00:06 +0100

    add team-02-database check

---
+++ nixos-configs/team02-vm-check-exporter.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+{
+  ## PROMETHEUS EXPORT
+  #  Warning: Make sure that psql is installed on the VM, as it is not checked here!
+  services.prometheus.exporters.script = {
+    enable = true;
+    port = 9100;
+    settings.scripts = [
+      # { name = "db-check"; script = "mysql -u team3 -p$mysqlRootPassword"; }
+      { name = "db-check"; script = "nc -zv 192.168.4.5 3306"; }
+    ];
+  };
+}
