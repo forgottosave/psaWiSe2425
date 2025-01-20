be31638a55bc0ee72683e39bc479f2288fda22eb
Timon Ensel <forgottosave.ensel@tum.de>
2025-01-20 18:28:15 +0100

    add vm10 docker settings

--- nixos-configs/team02-vm-check-exporter.nix
+++ nixos-configs/team02-vm-check-exporter.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }:
 {
+  ## DOCKER
+  virtualisation.docker.enable = true;
+  users.extraGroups.docker.members = [ "root" ];
+  
   ## PROMETHEUS EXPORT
   #  Warning: Make sure that nc is installed on the VM, as it is not checked here!
   services.prometheus.exporters.script = {
