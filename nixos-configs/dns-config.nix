2061f8c140646017e3c076f50e12fb6af7dc2aeb
Benjamin Liertz <benjamin.liertz@tum.de>
2025-01-21 12:22:06 +0100

    dns prometheus all zones

--- nixos-configs/dns-config.nix
+++ nixos-configs/dns-config.nix
@@ -8,11 +8,11 @@
             bind enp0s8
             root /etc/nixos/dns
             log
+            prometheus :9153
         }
         
         . {
             forward . 131.159.254.1 131.159.254.2
-            prometheus :9153 # Include metrics for this zone if desired
             import default
         }
         
