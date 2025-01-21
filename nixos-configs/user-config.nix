826f26a866270b9b7c8615da7cc3e3fd1eaf09fe
Benjamin Liertz <benjamin.liertz@tum.de>
2025-01-21 17:08:16 +0100

    changed home dir rights

--- nixos-configs/user-config.nix
+++ nixos-configs/user-config.nix
@@ -8,7 +8,7 @@
     home = "/home/ge95vir";  
     uid = 1010;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEXPasCKmYHeTJ06DBWXCaYYUVM/Euo+X5tU0WpGWxRt gedeon.lenz@tum.de" ];  
   };  
   users.users.ge43fim = {  
@@ -16,7 +16,7 @@
     home = "/home/ge43fim";  
     uid = 1011;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJxRi9ByDSdft3zbasPq04DvoDHZDKHLzg5vtP+Caii andrey.maleev@tum.de" ];  
   };  
   #Team2  
@@ -25,7 +25,7 @@
     home = "/home/ge78nes";  
     uid = 1020;  
     group = "students";
-    homeMode = "701";  
+    homeMode = "755";  
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8UlkVfMHSKssjMGGDOi79yNQC1OT81XB8fMk8EE9Mb nastja@w205-2c-v4.eduroam.dynamic.rbg.tum.de" ];  
   };  
   users.users.ge96hoj = {  
@@ -33,7 +33,7 @@
     home = "/home/ge96hoj";  
     uid = 1021;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUNGYlJSPF3pallfNKZoalknfH2Ep115DAJ0rxgqO6P louis@louis-ubuntu-2024" ];  
   };  
   #Team3  
@@ -42,7 +42,7 @@
     home = "/home/ge78zig";  
     uid = 1030;  
     group = "students";
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFKywkjovjz87VQHeNVSGUlc/5Nl4eH4Hj1SrYHIeqM" ];  
   };  
   users.users.ge96xok = {  
@@ -50,7 +50,7 @@
     home = "/home/ge96xok";  
     uid = 1031;  
     group = "students";
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBwkCLE+pDy8HvHy98MwsNH/sxPYmBRXuREOd2jTMXPV timon.ensel@tum.de" ];  
   };  
   #Team4  
@@ -59,7 +59,7 @@
     home = "/home/ge87yen";  
     uid = 1040;  
     group = "students";
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNOSacJR2QmqoyuTLVb7HMNceyZ1iJMysrLelGbKdt2 joana.arguirova@tum.de" ];  
   };  
   users.users.ge47sof = {  
@@ -67,7 +67,7 @@
     home = "/home/ge47sof";  
     uid = 1041;  
     group = "students"; 
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH28yN3KD2y7WNss5Kko8C8t0JShA2fMaFuqA4SD0xKB manuela.rosenlehner@tum.de" ];  
   };  
   #Team5  
@@ -76,7 +76,7 @@
     home = "/home/ge47kut";  
     uid = 1050;  
     group = "students";
-    homeMode = "701";  
+    homeMode = "755";  
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAF16c1yC1VO+Y3edoOpRGMnPTbwXnhQC3IFyT+8Rrvl oliver.duerer@tum.de" ];  
   };  
   users.users.ge87liq = {  
@@ -84,7 +84,7 @@
     home = "/home/ge87liq";  
     uid = 1051;  
     group = "students";
-    homeMode = "701";  
+    homeMode = "755";  
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqLECy9QVF4zJsPg/EqwHjDfBI1cid5XQquwCVf2xcR christian.krinitsin@tum.de" ];  
   };  
   #Team6  
@@ -93,7 +93,7 @@
     home = "/home/ge59pib";  
     uid = 1060;  
     group = "students"; 
-    homeMode = "701"; 
+    homeMode = "755"; 
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTyrsqSn9oAlqyThh1VoIqLoOzNV5a9IAeERC09fAFU im-in-your-walls" ];  
   };  
   users.users.ge65peq = {  
@@ -101,7 +101,7 @@
     home = "/home/ge65peq";  
     uid = 1061;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKmGZBpo2o5HMwSCOLVuznuaZ0ZdJgedaRyTYFxJzEK christian.sommer@tum.de" ];  
   };
   #Team7  
@@ -110,7 +110,7 @@
     home = "/home/ge63gut";   
     uid = 1070;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqGJj/qaJn7hULwBvUBdxwSij5dJ1l1ErFmYj8b7aVh sina.mozaffari-tabar@tum.de" ];  
   };  
   users.users.ge64baw = {   
@@ -118,7 +118,7 @@
     home = "/home/ge64baw";   
     uid = 1071;   
     group = "students";   
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID74Bxdsa6oQATpMu6sPJ/49J8KcG3wHDz2Wsgk8n+ZE mostafa.nejati.hatamian@tum.de" ];  
   };  
   #Team8  
@@ -127,7 +127,7 @@
     home = "/home/ge84zoj";   
     uid = 1080;  
     group = "students";   
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP11LUlgDrjK7Bf3jwmQuuUKkWLRgDXNPx9g4sn1+iC7 bjarne.hansen@proton.me" ];  
   };  
   users.users.ge94bob = {   
@@ -135,7 +135,7 @@
     home = "/home/ge94bob";   
     uid = 1081;  
     group = "students";   
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgT4clpBTjp7UeMqP/Qy1mchYBYAZPWuZ0xr9ZOiviG fabianluca.schulz@tum.de" ];  
   };  
   #Team9   
@@ -144,7 +144,7 @@
     home = "/home/ge87huk";   
     uid = 1090;  
     group = "students";   
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeIYnWtdhXEbQ+ZxiEd9Ad5/C4BdK60G8k7fJ4XAu34 ge87huk@mytum.de" ];  
   };  
   users.users.ge64wug = {   
@@ -152,7 +152,7 @@
     home = "/home/ge64wug";   
     uid = 1091;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEN53t0LaDrA2zQtW6jIk5xAndloBe09rXfbaS6QwXq+ sophie@Sophies-Mac.fritz.box" ];  
   };  
   #Team10  
@@ -161,7 +161,7 @@
     home = "/home/ge65hog";  
     uid = 1100;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEeWH4TQsfrcYjQWCylMHqOy3y/RsaahdAI0QdkiyUXJ katharina.schmenn@tum.de" ];  
   };  
   users.users.ge38hoy = {  
@@ -169,7 +169,7 @@
     home = "/home/ge38hoy";  
     uid = 1101;  
     group = "students";  
-    homeMode = "701";
+    homeMode = "755";
     openssh.authorizedKeys.keys = [ "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIK4f12ldfJGJhMUbAYOz5E3aXc+F6SScLb2n2KdVfqu4AAAAC3NzaDp0ZXJtaXVz" ];  
   };
 
