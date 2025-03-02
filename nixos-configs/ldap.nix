118a1368b1e3b5ef07521c59743679610e3f1e02
Timon Ensel <forgottosave.ensel@tum.de>
2025-03-02 12:28:06 +0100

    add first ldap steps

--- nixos-configs/ldap.nix
+++ nixos-configs/ldap.nix
@@ -5,10 +5,6 @@
     enable = true;
     package = pkgs.openldap;
     settings = {
-      # Define the base domain
-      "olcSuffix" = "dc=psa-team03,dc=in,dc=tum,dc=de";
-      "olcRootDN" = "cn=admin,dc=psa-team03,dc=in,dc=tum,dc=de";
-      "olcRootPW" = "{SSHA}your_encrypted_password";
     };
   };
 }
