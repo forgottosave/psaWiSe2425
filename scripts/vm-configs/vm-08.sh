0ad9df02be5a661c4f740a083c097f524e8fe997
Benjamin Liertz <benjamin.liertz@gmx.net>
2025-03-09 21:25:29 +0100

    another try

--- scripts/vm-configs/vm-08.sh
+++ scripts/vm-configs/vm-08.sh
@@ -47,10 +47,22 @@ sed_placeholders[root_access]='
 
 sed_placeholders[firewall]='
       # --- NFS (server) --- (only vm8)
+      # Allow rpcbind
       iptables -A INPUT -p tcp --dport 111 -s 192.168.0.0/16 -j ACCEPT
       iptables -A INPUT -p udp --dport 111 -s 192.168.0.0/16 -j ACCEPT
+
+      # Allow mountd (fixed to port 20048)
+      iptables -A INPUT -p tcp --dport 20048 -s 192.168.0.0/16 -j ACCEPT
+      iptables -A INPUT -p udp --dport 20048 -s 192.168.0.0/16 -j ACCEPT
+
+      # Allow NFS itself
       iptables -A INPUT -p tcp --dport 2049 -s 192.168.0.0/16 -j ACCEPT
       iptables -A INPUT -p udp --dport 2049 -s 192.168.0.0/16 -j ACCEPT
+
+      # Allow NLM (nlockmgr) for file locking if needed
+      iptables -A INPUT -p udp --dport 37373 -s 192.168.0.0/16 -j ACCEPT
+      iptables -A INPUT -p tcp --dport 43027 -s 192.168.0.0/16 -j ACCEPT
+
       
       # --- Samba (server) --- (only vm8)
       iptables -A INPUT -p udp --dport 137 -m conntrack --ctstate NEW -s 192.168.0.0/16 -j ACCEPT
